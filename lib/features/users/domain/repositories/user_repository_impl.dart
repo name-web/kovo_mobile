b176073eea840113358dc3293d1f47f4d6ca9424
name-web <fanta.doumouya@epitech.eu>
2026-08-09 12:47:39 +0000

    feat(users): implement user repository

---
+++ lib/features/users/domain/repositories/user_repository_impl.dart
@@ -0,0 +1,23 @@
+import '../../domain/entities/user.dart';
+import '../../domain/repositories/user_repository.dart';
+import '../../data/models/user_model.dart';
+import '../../../../core/network/api_client.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final ApiClient apiClient;
+
+  UserRepositoryImpl({
+    required this.apiClient,
+  });
+
+  @override
+  Future<User> getProfile() async {
+    final response = await apiClient.get<Map<String, dynamic>>(
+      '/profile',
+    );
+
+    final userModel = UserModel.fromJson(response.data!);
+
+    return userModel.toEntity();
+  }
+}
